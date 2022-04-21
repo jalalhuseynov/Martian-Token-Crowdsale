5694dfb2e6b2e56bd710ec1b4bdbb135bcb1d84f
jalalhuseynov <91158293+jalalhuseynov@users.noreply.github.com>
2022-04-21 19:04:53 -0400

    Update Capped_KaseiCoinCrowdsale_capped.sol
    minor change on contract -deleting old path for KaseiCoin-

--- Capped_KaseiCoinCrowdsale_capped.sol
+++ Capped_KaseiCoinCrowdsale_capped.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.5.5;
 
-import "./Copy_KaseiCoin.sol";
+import "./KaseiCoin.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/Crowdsale.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/emission/MintedCrowdsale.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/validation/CappedCrowdsale.sol";
